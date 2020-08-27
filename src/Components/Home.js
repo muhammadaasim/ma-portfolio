88b6742a2f5c3a04fe6cde2ffc2094bf5811c886
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-08-28 02:08:54 +0500

    Added Type Writer Effect to text

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,30 +2,47 @@ import React from 'react'
 import './theme.css'
 import mwl from './mwl.svg'
 import { Container, Col, Row } from 'react-bootstrap'
+import useWebAnimations, { flipInX } from "@wellyshen/use-web-animations";
+import Typewriter from 'typewriter-effect';
+
 const Home = () => {
+    const { ref, playState } = useWebAnimations({ ...flipInX });
     return (
         <div className="bg-s h-p">
             <Container>
                 <Row>
                     <Col className="h-text " xs={12} lg={6} md={7}  >
-                        <div >
+                        <div ref={ref}>
                             <h3>
                                 Muhammad Aasim
-                        </h3>
+                            </h3>
                             <p className="text-left">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                   Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                   Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                            
-                        </p>
+
+                                <Typewriter
+                                    options={{
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                    onInit={(typewriter) => {
+                                        typewriter.typeString('Hello there! I am Web Developer who knows HTML,CSS,JS,Bootstrap, React.js')
+                                            .callFunction(() => {
+                                                console.log('String typed out!');
+                                            })
+                                            .pauseFor(2500)
+                                            .deleteAll()
+                                            .callFunction(() => {
+                                                console.log('All strings were deleted');
+                                            })
+                                            .start();
+                                    }}
+                                />
+
+                            </p>
                         </div>
                     </Col>
 
                     <Col xs={12} lg={6} md={5}>
-                        <img src={mwl} alt="Man With Laptop"/>
+                        <img src={mwl} alt="Man With Laptop" />
                     </Col>
                 </Row>
             </Container>
---
+++ src/Components/Services.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap'
+const Services = () => {
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                    </Col>
+                    <Col>
+                    </Col>
+                    <Col>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Services

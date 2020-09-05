55c878785a8296b59d8f229e10ace4534f62977f
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-09-05 02:49:41 -0700

    Added MuiPhoneNumber

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import { FormControl, TextField, Button, Snackbar, makeStyles } from '@material-ui/core';
+import { FormControl, TextField, Button, Snackbar } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 import ScrollAnimation from 'react-animate-on-scroll';
 import CU from './imgs/contactus.svg'
 import useWebAnimations from "@wellyshen/use-web-animations";
-
+import MuiPhoneNumber from 'material-ui-phone-number'
 import './theme.css'
 const Contact = () => {
     const { ref: illustration, getAnimation } = useWebAnimations({
@@ -28,59 +28,59 @@ const Contact = () => {
     }
 
 
-        const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-        const handleClick = () => {
-            setOpen(true);
-        };
+    const handleClick = () => {
+        setOpen(true);
+    };
 
-        const handleClose = (event, reason) => {
-            if (reason === 'clickaway') {
-                return;
-            }
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
 
-            setOpen(false);
-        };
+        setOpen(false);
+    };
 
-        return (
-            <div id="contact">
-                <Container>
-                    <Row>
-                        <Col>
-                            <ScrollAnimation animateIn="fadeInUp">
-                                <h3 className="text-center ppt-5 mt-4 pb-3">Contact Me</h3>
-                            </ScrollAnimation>
-                        </Col>
-                    </Row>
-                    <Row>
+    return (
+        <div id="contact">
+            <Container>
+                <Row>
+                    <Col>
+                        <ScrollAnimation animateIn="fadeInUp">
+                            <h3 className="text-center ppt-5 mt-4 pb-3">Contact Me</h3>
+                        </ScrollAnimation>
+                    </Col>
+                </Row>
+                <Row>
 
-                        <Col xs={12} sm={12} md={7} lg={7} xl={7} className="pb-5">
-                            <img src={CU} alt="Contact US" ref={illustration} onMouseEnter={() => getAnimation().pause()} onMouseLeave={() => getAnimation().play()} />
-                        </Col>
-                        <Col xs={12} sm={12} md={5} lg={5} xl={5}>
-                            <div className="contactform">
-                                <FormControl>
-                                    <div className="mb-1">
-                                        <TextField id="input-with-icon-grid" label="First Name" className="txtname mr-2" />
-                                        <TextField id="input-with-icon-grid" label="Last Name" className="txtname mb-1" />
-                                    </div>
-                                    <TextField id="input-with-icon-grid" label="Email" className="txtbox mb-1" />
-                                    <TextField id="input-with-icon-grid" label="Phone Number" className="txtbox mb-1" />
-                                    <TextField id="input-with-icon-grid" multiline rows={4} label="Message" className="txtbox mb-3" />
-                                    <Button variant="contained" color="primary" style={{ width: 130 }} onClick={handleClick}>
-                                        Send
+                    <Col xs={12} sm={12} md={7} lg={7} xl={7} className="pb-5">
+                        <img src={CU} alt="Contact US" ref={illustration} onMouseEnter={() => getAnimation().pause()} onMouseLeave={() => getAnimation().play()} />
+                    </Col>
+                    <Col xs={12} sm={12} md={5} lg={5} xl={5}>
+                        <div className="contactform">
+                            <FormControl>
+                                <div className="mb-1">
+                                    <TextField id="input-with-icon-grid" label="First Name" className="txtname mr-2" />
+                                    <TextField id="input-with-icon-grid" label="Last Name" className="txtname mb-1" />
+                                </div>
+                                <TextField id="input-with-icon-grid" label="Email" className="txtbox mb-3" />
+                                <MuiPhoneNumber defaultCountry={'pk'} className="txtbox mb-2" />
+                                <TextField id="input-with-icon-grid" multiline rows={4} label="Message" className="txtbox mb-3" />
+                                <Button variant="contained" color="primary" style={{ width: 130 }} onClick={handleClick}>
+                                    Send
                                     </Button>
-                                </FormControl>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-                <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
-                    <Alert onClose={handleClose} severity="success">
-                        This is a success message!
+                            </FormControl>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Snackbar open={open} autoHideDuration={5000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    This is a success message!
         </Alert>
-                </Snackbar>
-            </div>
-        )
+            </Snackbar>
+        </div>
+    )
 }
-    export default Contact;
+export default Contact;
